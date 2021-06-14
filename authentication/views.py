e3dbb207270a21ed92c2b2fe8c2238de56cf61f3
Deneshraj <34793108+Deneshraj@users.noreply.github.com>
2021-06-14 11:59:24 +0530

    Revert "Posts"

--- authentication/views.py
+++ authentication/views.py
@@ -1,27 +1,8 @@
-from utils import response
-from utils.helper import *
+from django.http.response import JsonResponse
 
 def login(request, *args, **kwargs):
-    return response.failure
+    print(request.headers)
 
-def signup(request, *args, **kwargs):
-    query = request.POST
-
-    email = query.get(key="email")
-    first_name = query.get(key="first_name")
-    last_name = query.get(key="last_name")
-    username = query.get(key="username")
-    password = query.get(key="password")
-
-    return createuser(username, email, password, first_name, last_name)
-
-def checkemailid(request, *args, **kwargs):
-    if EmailExists(request.GET.get(key="email")):
-        return response.success
-    return response.failure
-
-def checkusername(request, *args, **kwargs):
-    if UsernameExists(request.GET.get(key="username")):
-        return response.success
-    return response.failure
-
+    return JsonResponse({
+        "msg": "Hello, world!"
+    })
--- utils/response.py
+++
@@ -1,7 +0,0 @@
-from django.http.response import JsonResponse
-
-success = JsonResponse({"status": "success"})
-failure = JsonResponse({"status": "failure"})
-
-def sendstatus(status_msg):
-    return JsonResponse({"status": status_msg})
